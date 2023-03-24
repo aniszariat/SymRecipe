ce83424b5a995dad48abf36d732f51779d638e32
aniszariat <zariatanis.az@gmail.com>
2023-03-24 16:06:59 +0100

    create fake elements and send them to DB

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Ingredient;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
+
+class AppFixtures extends Fixture
+{
+    /**
+     * @var Generator
+     */
+    private Generator $faker ;
+    public function __construct()
+    {
+        $this->faker = Factory::create('en_EN');
+    }
+
+
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        for ($i=0; $i <10 ; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($this->faker->word())
+            ->setPrice(mt_rand(0, 200)) ;
+            $manager->persist($ingredient);
+        }
+        $manager->flush();
+    }
+}
